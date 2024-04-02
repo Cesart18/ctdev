277514903376670c81102f541602be0deedc8a3c
Cesar Tovar <cjataonline@gmail.com>
2024-04-02 16:01:26 -0400

    cambie el tipo de letra para los p, a y botones, modifique el hero mediaquery y estilice el footer

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,38 +1,51 @@
 export const SKILLS = [
     {
         name: 'html',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/html'
     },
     {
         name: 'css',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/css',
     },
     {
         name: 'javascript',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
     },
     {
         name: 'typescript',
+        url: 'https://www.typescriptlang.org/'
     },
     {
         name: 'astro',
+        url: 'https://astro.build/'
+
     },
     {
         name: 'react',
+        url: 'https://react.dev'
     },
     {
         name: 'dart',
+        url: 'https://dart.dev/'
     },
     {
         name: 'flutter',
+        url: 'https://flutter.dev/'
     },
     {
         name: 'git',
+        url: 'https://git-scm.com/'
     },
     {
         name: 'github',
+        url: 'https://github.com/'
     },
     {
         name: 'figma',
+        url: 'https://www.figma.com/'
     },
     {
         name: 'SEO',
+        url: 'https://developers.google.com/search/docs/fundamentals/seo-starter-guide?hl=es-419#:~:text=SEO%E2%80%94short%20for%20search%20engine,site%20through%20a%20search%20engine.'
     },
 ]
