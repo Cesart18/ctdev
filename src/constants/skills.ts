500143ffea8a85fe63fab1ff4b899f9c27219b83
Cesar Tovar <cjataonline@gmail.com>
2024-04-02 10:27:56 -0400

    agregue los icons faltantes de skills, y modifique el cursor en el hero

---
+++ src/constants/skills.ts
@@ -0,0 +1,38 @@
+export const SKILLS = [
+    {
+        name: 'html',
+    },
+    {
+        name: 'css',
+    },
+    {
+        name: 'javascript',
+    },
+    {
+        name: 'typescript',
+    },
+    {
+        name: 'astro',
+    },
+    {
+        name: 'react',
+    },
+    {
+        name: 'dart',
+    },
+    {
+        name: 'flutter',
+    },
+    {
+        name: 'git',
+    },
+    {
+        name: 'github',
+    },
+    {
+        name: 'figma',
+    },
+    {
+        name: 'SEO',
+    },
+]
