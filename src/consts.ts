c56bb3fd0fd60f9d11cefc59edd7bf802edd90fd
Cesar Tovar <cjataonline@gmail.com>
2024-04-03 09:56:25 -0400

    agregue el archivo types y consts y modifique su referente

---
+++ src/consts.ts
@@ -0,0 +1,68 @@
+import type { Links, Skills } from "./types";
+
+export const LINKS: Links[] = [
+    {
+        TEXT: 'Inicio',
+        HREF: '/'
+    },
+    {
+        TEXT: 'Proyectos',
+        HREF: '/proyectos/'
+    },
+    {
+        TEXT: 'Contacto',
+        HREF: '/contacto/'
+    },
+]
+
+export const SKILLS: Skills[] = [
+    {
+        NAME: 'html',
+        HREF: 'https://developer.mozilla.org/en-US/docs/Web/html'
+    },
+    {
+        NAME: 'css',
+        HREF: 'https://developer.mozilla.org/en-US/docs/Web/css',
+    },
+    {
+        NAME: 'javascript',
+        HREF: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+    },
+    {
+        NAME: 'typescript',
+        HREF: 'https://www.typescriptlang.org/'
+    },
+    {
+        NAME: 'astro',
+        HREF: 'https://astro.build/'
+
+    },
+    {
+        NAME: 'react',
+        HREF: 'https://react.dev'
+    },
+    {
+        NAME: 'dart',
+        HREF: 'https://dart.dev/'
+    },
+    {
+        NAME: 'flutter',
+        HREF: 'https://flutter.dev/'
+    },
+    {
+        NAME: 'git',
+        HREF: 'https://git-scm.com/'
+    },
+    {
+        NAME: 'github',
+        HREF: 'https://github.com/'
+    },
+    {
+        NAME: 'figma',
+        HREF: 'https://www.figma.com/'
+    },
+    {
+        NAME: 'SEO',
+        HREF: 'https://developers.google.com/search/docs/fundamentals/seo-starter-guide?hl=es-419#:~:text=SEO%E2%80%94short%20for%20search%20engine,site%20through%20a%20search%20engine.'
+    },
+]
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type Socials = {
+    NAME: string,
+    ICON: string,
+    HREF: string,
+}
+
+export type Links = {
+    TEXT: string,
+    HREF: string,
+}
+
+export type Skills = {
+    NAME: string,
+    HREF: string,
+}
