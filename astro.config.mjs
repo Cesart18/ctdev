ee7b1315a33d88d2c922b2680687d4d52782c38d
Cesar Tovar <cjataonline@gmail.com>
2024-04-13 14:21:57 -0400

    arregle el google analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ctdev.online/',
   integrations: [icon(), react({
     experimentalReactChildren: true
-  }), sitemap()],
+  }), sitemap(), partytown()]
 });
